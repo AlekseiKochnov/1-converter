9c3e490450501d8b6145ebbf2a8d525f1b1965b1
Aleksei <109661672+AlekseiKochnov@users.noreply.github.com>
2025-10-26 13:24:21 +0300

    Пакеты

--- 3-Pin/main.go
+++ 3-Pin/main.go
@@ -1,38 +1,5 @@
 package main
 
-import (
-	"errors"
-	"time"
-)
-
-type Bin struct {
-	Id        string
-	Private   bool
-	CreatedAt time.Time
-	Name string
-}
-
-type BinList []Bin
-
-func NewBin(id string, private bool, createdAt time.Time, name string) (*Bin, error) {
-	if id == "" || name == "" {
-		return nil, errors.New("empty string (id or name)")
-	}
-
-	b := Bin{
-		Id: id,
-		Private: private,
-		CreatedAt: createdAt, 
-		Name: name,
-	}
-	return &b, nil
-}
-
-func NewBinList() *BinList {
-	return &BinList{}
-}
-
 func main() {
-
 
 }
