e9e44bb31eb1bdccdda00bb02d821cb692838ece
Aleksei <109661672+AlekseiKochnov@users.noreply.github.com>
2025-10-31 12:30:32 +0300

    Работа с ОС

--- 3-Pin/file/file.go
+++ 3-Pin/file/file.go
@@ -1,1 +1,22 @@
 package file
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+func ReadJsonFile(nameFile string) ([]byte, error) {
+
+    if !strings.Contains(nameFile, ".json") {
+		return nil, errors.New("расширение файла не json")
+    }	
+
+	jsonData, err := os.ReadFile(nameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+
+}
