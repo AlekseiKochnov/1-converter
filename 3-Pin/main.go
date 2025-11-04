2167a3e786354b9c074dd423a7126722d70b416d
Aleksei <109661672+AlekseiKochnov@users.noreply.github.com>
2025-11-04 18:30:55 +0300

    Интерфейсы

--- 3-Pin/main.go
+++ 3-Pin/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"3-PIN/bins"
+	"3-PIN/file"
+	"3-PIN/storage"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
 func main() {
+
+	bin, err := bins.NewBin("1", false, time.Now(), "one")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	List := bins.NewBinList()
+	binList := append(*List, *bin)
+	data, err := json.Marshal(binList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}	
+	err = storage.SaveBin(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	js, err := file.ReadJsonFile("Bin.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(js))
 
 }
